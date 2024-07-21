fbb6e1701d492b4ba8141a04d8c264fee323f861
bismakhan23 <bismafarhan576@gmail.com>
2024-07-22 00:04:28 +0500

    first commit

---
+++ chapter=21-25/app.js
@@ -0,0 +1,79 @@
+//answer no 1:
+
+var firstName=prompt("Enter your first name")
+var lastName=prompt("Enter your last name")
+var fullName=firstName.concat(lastName)
+console.log(fullName)
+
+
+//answer no 2:
+
+var mobilePhoneModel=prompt("Enter your favorite mobile phone model")
+var findLEngth=mobilePhoneModel.length
+console.log(findLEngth)
+
+//answer no 3:
+
+var natioNAlity="PAKISTANI"
+var findINdexof=natioNAlity.indexOf("N")
+document.write(findINdexof)
+
+//ANSWER NO 4
+
+var heLLo="HELLO WORLD"
+var findINdex=heLLo.indexOf("L")
+document.write(findINdex)
+
+//ANSWER NO 5
+
+var hello="PAKISTANI"
+var fiND=hello.charAt(3)
+document.write(fiND)
+
+//ANSWER NO 6:
+
+var firstName=prompt("Enter your first name")
+var lastName=prompt("Enter your last name")
+var fullName=firstName.concat(lastName)
+console.log(fullName)
+
+//ANSWER NO 7:
+
+var city="HYDERABAD"
+var finalREPLCECity=city.replace("HYDERABAD","ISLAMABAD")
+console.log(finalREPLCECity)
+
+//ANSWER NO 8:
+
+var message = "Ali and Sami are best friends. They play cricket and football together."
+var finalREPLCEand=message.replace("and","&")
+console.log(finalREPLCEand)
+
+//ANSWER NO 9:
+
+var num=472
+var num2=num.toString()
+document.write(num2+472)
+
+//ANSWER NO 10:
+
+var input=prompt("Enter your fav dry fruit name","peanuts etc.")
+var finalINput=input.toUpperCase()
+console.log(finalINput)
+
+//ANSWE NO 11:
+
+// var userinput=prompt("ENTER YOU FAV LANGUAGE","javascript etc.")
+// var finalINPUt=userinput.
+
+
+//ANSWER NO 12:
+
+var num3=35.36
+var num4=num3.substr(2)
+console.log(num4)
+
+//ANSWER NO 13:
+
+
+
---
+++ chapter=26-30/app.js
@@ -0,0 +1,60 @@
+//answer no 1:
+//part a:
+
+var nuMBer=3.4521
+console.log(nuMBer)
+//part b:
+
+var roUNd=Math.round(nuMBer)
+console.log(roUNd)
+
+//part c:
+
+var roUNd=Math.floor(nuMBer)
+console.log(roUNd)
+
+//part d:
+
+var roUNd=Math.ceil(nuMBer)
+console.log(roUNd)
+
+//answer no 2:
+//part a:
+var nUmber=-2.673
+console.log(nUmber)
+
+//part b:
+var roundoff=Math.round(nUmber)
+console.log(roundoff)
+
+//part c:
+var floorvalUE=Math.floor(nUmber)
+console.log(floorvalUE)
+
+//part d:
+var ceilVAlue=Math.ceil(nUmber)
+console.log(ceilVAlue)
+
+//answer no 3:
+
+var a =Math.abs(-4)
+console.log(a)
+
+var b=Math.abs(5)
+console.log(b)
+
+//answer no 4:
+
+var raNdom=Math.floor(Math.random()*6)+1
+console.log(raNdom)
+
+
+//answer no 5
+var rAndOm=Math.floor(Math.random()*2)+1
+console.log(rAndOm)
+
+//answer no 6
+var ranDOm=Math.floor(Math.random()*100)+1
+console.log(ranDOm)
+
+//answer no 7:
